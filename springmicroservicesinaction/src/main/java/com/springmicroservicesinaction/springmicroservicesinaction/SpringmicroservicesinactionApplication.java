9cc01438601a79d55f4cb1949c4cb07556a4f531
Hamza Iddaoui <hiddaoui@sqli.com>
2024-09-07 18:14:18 +0100

    init project

---
+++ springmicroservicesinaction/src/main/java/com/springmicroservicesinaction/springmicroservicesinaction/SpringmicroservicesinactionApplication.java
@@ -0,0 +1,13 @@
+package com.springmicroservicesinaction.springmicroservicesinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicroservicesinactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmicroservicesinactionApplication.class, args);
+	}
+
+}
